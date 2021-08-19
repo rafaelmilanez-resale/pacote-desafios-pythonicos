de75cd9993e6e2cf32e882f0216c8294a93c2d6b
rafaelmilanez <rafael.milanez@resale.com>
2021-08-19 20:36:18 -0300

    feat: complete 05_verbing.py challenge

--- 05_verbing.py
+++ 05_verbing.py
@@ -11,8 +11,12 @@ Retorne o resultado da string.
 """
 
 def verbing(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) < 3:
+        return s
+    elif s[-3:] == "ing":
+        return s + "ly"
+
+    return s + "ing"
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
