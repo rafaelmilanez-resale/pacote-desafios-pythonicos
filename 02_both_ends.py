f4457932f2bba085606ecac791d810ad058c8b3b
rafaelmilanez <rafael.milanez@resale.com>
2021-08-19 20:17:08 -0300

    feat: complete 02_both_ends.py challenge

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -8,8 +8,10 @@ for menor que 2, retorne uma string vazia.
 """
 
 def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) < 2:
+        return ''
+
+    return s[:2] + s[-2:]
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
